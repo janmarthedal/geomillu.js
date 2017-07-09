7ae4c3f6e29bf75f34ae6ab86f6ab7dc5ecd0335
Jan Marthedal Rasmussen <jan@janmr.com>
2017-07-09 20:29:17 +0200

    NodeWriter -- WIP

--- illunode.ts
+++ illunode.ts
@@ -8,8 +8,42 @@ export interface DrawOptions {
     'stroke-width'?: length;
 }
 
-class Node {
+export abstract class NodeWriter {
+    abstract begin(): void;
+    abstract end(): void;
+    abstract beginAttr(attr: DrawOptions): void;
+    abstract endAttr(): void;
+    abstract beginTransform(a: number, b: number, c: number,
+        d: number, e: number, f: number): void;
+    abstract endTransform(): void;
+}
+
+export class DebugNodeWriter extends NodeWriter {
+    indent: string = '';
+    begin(): void {
+        this.indent = '';
+    }
+    end(): void {}
+    beginAttr(attr: DrawOptions): void {
+        console.log(this.indent + 'attr' + 
+            Object.keys(attr).map(k => ` ${k}="${attr[k]}"`).join(''));
+        this.indent += '  ';
+    }
+    endAttr(): void {
+        this.indent = this.indent.substr(2);
+    }
+    beginTransform(a: number, b: number, c: number, d: number, e: number, f: number): void {
+        console.log(this.indent + `transform ${a} ${b} ${c} ${d} ${e} ${f}`);
+        this.indent += '  ';
+    }
+    endTransform(): void {
+        this.indent = this.indent.substr(2);
+    }
+}
+
+export abstract class Node {
     //outline: Polygon;
+    abstract write(writer: NodeWriter): void;
 }
 
 class ObjectNode extends Node {
@@ -18,14 +52,23 @@ class ObjectNode extends Node {
         super();
         this.obj = obj;
     }
+    write(writer: NodeWriter): void {
+    }
 }
 
 class InternalNode extends Node {
     children: Node[];
+    constructor() {
+        super();
+        this.children = [];
+    }
     add(node: Node|GeomObject) {
         if (node instanceof GeomObject)
             node = new ObjectNode(node);
         this.children.push(node);
+    }
+    write(writer: NodeWriter): void {
+        this.children.forEach(c => c.write(writer));
     }
 }
 
@@ -35,6 +78,11 @@ export class AttrNode extends InternalNode {
         super();
         this.attr = attr;
     }
+    write(writer: NodeWriter): void {
+        writer.beginAttr(this.attr);
+        super.write(writer);
+        writer.endAttr();
+    }    
 }
 
 export class TransformNode extends InternalNode {
@@ -47,4 +95,9 @@ export class TransformNode extends InternalNode {
         super();
         this.t = [a, b, c, d, e, f];
     }
+    write(writer: NodeWriter): void {
+        writer.beginTransform(this.t[0], this.t[1], this.t[2], this.t[3], this.t[4], this.t[5]);
+        super.write(writer);
+        writer.endTransform();
+    }    
 }
--- mj-test.ts
+++ mj-test.ts
@@ -1,7 +1,7 @@
 /// <reference types="jsdom" />
 
 import * as mjAPI from 'mathjax-node';
-import {AttrNode, TransformNode} from './illunode';
+import {AttrNode, TransformNode, Node, DebugNodeWriter} from './illunode';
 
 mjAPI.config({
   MathJax: {
@@ -12,12 +12,27 @@ mjAPI.start();
 
 const math = 'E = mc^2';
 
-function makeTransformNode(v: string): TransformNode {
-    const match = v.match(/transform\( *(\d+) +(\d+) +(\d+) +(\d+) +(\d+) +(\d+) *\)/);
+function parseTransform(v: string, outNode: AttrNode|TransformNode): TransformNode {
+    let match = v.match(/matrix\( *([-.0-9e]+) +([-.0-9e]+) +([-.0-9e]+) +([-.0-9e]+) +([-.0-9e]+) +([-.0-9e]+) *\)/);
     if (match) {
-        return new TransformNode(parseFloat(match[1]), parseFloat(match[2]), parseFloat(match[3]), parseFloat(match[4]), parseFloat(match[5]), parseFloat(match[6]));
+        const tnode = new TransformNode(parseFloat(match[1]), parseFloat(match[2]), parseFloat(match[3]), parseFloat(match[4]), parseFloat(match[5]), parseFloat(match[6]));
+        outNode.add(tnode);
+        return tnode;
     }
-    throw new Error('Unsupported transform attribute');
+    match = v.match(/translate\( *([-.0-9e]+),([-.0-9e]+) *\)/);
+    if (match) {
+        const tnode = new TransformNode(1, 0, 0, 1, parseFloat(match[1]), parseFloat(match[2]));
+        outNode.add(tnode);
+        return tnode;
+    }
+    match = v.match(/scale\( *([-.0-9e]+) *\)/);
+    if (match) {
+        const s = parseFloat(match[1]);
+        const tnode = new TransformNode(s, 0, 0, s, 0, 0);
+        outNode.add(tnode);
+        return tnode;
+    }
+    throw new Error(`Unsupported transform attribute "${v}"`);
 }
 
 function parseSVGNode(node: Element, outNode: AttrNode|TransformNode) {
@@ -25,11 +40,13 @@ function parseSVGNode(node: Element, outNode: AttrNode|TransformNode) {
     while (c !== null) {
         switch (c.tagName.toLowerCase()) {
             case 'g':
+                let subNode;
                 if (c.hasAttribute('transform')) {
-                    const tnode = makeTransformNode(c.getAttribute('transform'));
-                    outNode.add(tnode);
-                    outNode = tnode;
+                    subNode = parseTransform(c.getAttribute('transform'), outNode);
                 }
+                if (subNode)
+                    parseSVGNode(c, subNode);
+                break;
             default:
                 console.log('Ignoring node ' + c.tagName);
         }
@@ -43,6 +60,7 @@ function parseSVG(root: Element) {
         throw new Error('Not svg node');
     const outRoot = new AttrNode();
     parseSVGNode(root, outRoot);
+    return outRoot;
 }
 
 mjAPI.typeset({
@@ -52,6 +70,8 @@ mjAPI.typeset({
     svgNode: true
 }, function (data) {
     if (!data.errors) {
-        parseSVG(data.svgNode);
+        const node = parseSVG(data.svgNode);
+        const writer = new DebugNodeWriter();
+        node.write(writer);
     }
 });
